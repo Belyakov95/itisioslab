a28a04cd37ceaf5ee4ff8710b211e37994cea300
itisioslab <itisioslab@Mac-mini-administrator-77.local>
2014-08-19 10:36:32 +0400

    Initial Commit

---
+++ project2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  project2
+//
+//  Created by itisioslab on 19.08.14.
+//  Copyright (c) 2014 qwerty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ project2Tests/project2Tests.m
@@ -0,0 +1,34 @@
+//
+//  project2Tests.m
+//  project2Tests
+//
+//  Created by itisioslab on 19.08.14.
+//  Copyright (c) 2014 qwerty. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface project2Tests : XCTestCase
+
+@end
+
+@implementation project2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
